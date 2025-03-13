d1fe25cde5230a38bd03f560bb5ed929c1d20d02
Hou, Gloria <gloria@layer6.ai>
2025-03-12 20:13:10 -0400

    run formatter

--- src/common/file_manager.py
+++ src/common/file_manager.py
@@ -4,6 +4,7 @@ from PyPDF2 import PdfReader
 from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+
 class FileManager:
     def __init__(self, file_path, chunk_size=2000):
         self.file_path = file_path
@@ -12,7 +13,9 @@ class FileManager:
         directory = os.path.dirname(file_path)
         base_name = os.path.splitext(os.path.basename(file_path))[0]
         self.texts_file = os.path.join(directory, f"{base_name}_texts.json")
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.chunk_size, chunk_overlap=200)
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size, chunk_overlap=200
+        )
 
         # Load texts from file if it exists
         if os.path.exists(self.texts_file):
@@ -28,8 +31,8 @@ class FileManager:
             page_text = page.extract_text()
             if page_text:  # Ensure the page has text
                 document = Document(
-                    metadata={'source': self.file_path, 'page': page_num},
-                    page_content=page_text
+                    metadata={"source": self.file_path, "page": page_num},
+                    page_content=page_text,
                 )
                 documents.append(document)
 
@@ -44,22 +47,24 @@ class FileManager:
 
     def process_pdf(self):
         data = self.load_pdf_document()
-        
+
         documents = self.text_splitter.split_documents(data)
         self.dump_documents(documents)
 
         return self.texts
-    
+
     def process_wiki_embedding(self):
         if not self.texts:
-            with open(self.file_path, 'r', encoding='utf-8') as f:
-            # Load the file content as a dictionary
+            with open(self.file_path, "r", encoding="utf-8") as f:
+                # Load the file content as a dictionary
                 data = json.load(f)
                 documents = []
                 for title, texts in data.items():
                     # Create embeddings for each text
                     for text in texts:
-                        doc = Document(page_content=title+": "+text, metadata={'source': title, 'file_path': self.file_path})
+                        doc = Document(
+                            page_content=title + ": " + text,
+                            metadata={"source": title, "file_path": self.file_path},
+                        )
                         documents.append(doc)
                 self.dump_documents(documents)
-    
